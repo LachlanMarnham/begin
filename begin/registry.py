0a6f171124134ed0bcb912eaa74a6de4bd2811ee
Lachlan Marnham <lachlan.marnham@farfetch.com>
2022-01-13 20:01:23 +0000

    Add Target and TargetMetaData

--- begin/registry.py
+++ begin/registry.py
@@ -1,3 +1,35 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class TargetMetaData:
+    function_name: str
+    registry_namespace: str
+
+    @classmethod
+    def create(cls, function, registry_namespace):
+        return cls(
+            function_name=function.__name__,
+            registry_namespace=registry_namespace,
+        )
+
+
+class Target:
+    def __init__(self, function, registry_namespace):
+        self._function = function
+        self._registry_namespace = registry_namespace
+        self._metadata = TargetMetaData.create(
+            function=self._function,
+            registry_namespace=self._registry_namespace,
+        )
+
+    def execute(self):
+        self.function()
+
+    def __hash__(self):
+        return hash(self._metadata)
+
+
 class Registry:
 
     def __init__(self, name='default'):
@@ -15,6 +47,8 @@ class Registry:
             return decorator
 
     def _register_target(self, function, **kwargs):
-        print(kwargs)
-        print(function.__name__)
+        new_target = Target(
+            function=function,
+            registry_namespace=self.name,
+        )
         self.targets[(self.name, function.__name__)] = function
