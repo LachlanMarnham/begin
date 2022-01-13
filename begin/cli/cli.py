3de1defe2ab1192d3cc32d1b56fc114cbb77f363
Lachlan Marnham <lachlan.marnham@farfetch.com>
2022-01-13 03:06:35 +0000

    Add cli.get_targets_path

--- begin/cli/cli.py
+++ begin/cli/cli.py
@@ -1,5 +1,24 @@
-import sys
+from pathlib import Path
+import importlib.util
+from begin.scripts.pymake import Registry
 
 
-def main():
-    print(sys.argv)
+def get_targets_paths():
+    cwd = Path.cwd()
+    yield from cwd.rglob('*targets.py')
+    # global_targets_dir = Path.home().joinpath('.begin')
+    # if global_targets_dir.exists():
+    #     yield from global_targets_dir.rglob('*targets.py')
+
+
+def load_targets():
+    targets = []
+    for path in get_targets_paths():
+        spec = importlib.util.spec_from_file_location("module.name", path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        for item in dir(module):
+            attribute = getattr(module, item)
+            if isinstance(attribute, Registry):
+                targets.append(attribute)
+    return targets
