905e57631c360d0ce1708e0c85b2ace9b9e0f521
Lachlan Marnham <lachlan.marnham@farfetch.com>
2022-01-13 03:12:22 +0000

    Add registry.py

---
+++ begin/registry.py
@@ -0,0 +1,20 @@
+class Registry:
+
+    def __init__(self, name='default'):
+        self.name = name
+        self.targets = {}
+
+    def register_target(self, *args, **kwargs):
+        if args:
+            function = args.pop()
+            self._register_target(function)
+        else:
+            def decorator(function):
+                self._register_target(function, **kwargs)
+                return function
+            return decorator
+    
+    def _register_target(self, function, **kwargs):
+        print(kwargs)
+        print(function.__name__)
+        self.targets[(self.name, function.__name__)] = function
