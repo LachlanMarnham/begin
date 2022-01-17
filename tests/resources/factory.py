41242990719e5c1b8d28811ec268015539b3109d
Lachlan Marnham <lachlan.marnham@farfetch.com>
2022-01-17 18:27:22 +0000

    Add factory.make_random_function_name

--- tests/resources/factory.py
+++ tests/resources/factory.py
@@ -2,6 +2,8 @@ from abc import (
     ABCMeta,
     abstractmethod,
 )
+import string
+import random
 
 from begin.registry import Target
 
@@ -13,6 +15,25 @@ class AbstractFactory(metaclass=ABCMeta):
     def create(self):
         pass
 
+
+def make_random_function_name():
+    """ Function names can contain uppercase letters, lowercase letters, numbers and
+    underscores, but cannot begin with a number. We will arbitrarily limit function names
+    to 20 characters. We will make their minimum length to 10 characters to avoid clashes
+    with other stuff in the namespace. """
+    allowed_first_characters = string.ascii_letters + '_'
+    allowed_characters = allowed_first_characters + string.digits
+
+    string_length = random.randint(10, 20)
+    function_name = random.choice(allowed_first_characters)
+    function_name += ''.join(random.choice(allowed_characters) for _ in range(string_length - 1))
+
+    return function_name
+
+
+def create_function(fn_name=None):
+    fn_name = fn_name if fn_name is not None else make_random_function_name()
+    exec(f'def {fn_name}(): pass')
 
 class TargetFactory(AbstractFactory):
 
@@ -26,3 +47,4 @@ class TargetFactory(AbstractFactory):
 # function_name = 'abc'
 # exec(f'def {function_name}(): pass')
 # fn = locals()['abc']
+import pdb; pdb.set_trace()
