e0aadb5786aa0b16df0ccccce98d77c792d6f2a8
Lachlan Marnham <lachlan.marnham@farfetch.com>
2022-01-17 20:13:33 +0000

    Add factory.make_random_dir_path

--- tests/resources/factory.py
+++ tests/resources/factory.py
@@ -4,6 +4,8 @@ from abc import (
     ABCMeta,
     abstractmethod,
 )
+from pathlib import Path
+from typing import Callable
 
 from begin.registry import Target
 
@@ -16,7 +18,7 @@ class AbstractFactory(metaclass=ABCMeta):
         pass
 
 
-def make_random_function_name():
+def make_random_function_name() -> str:
     """ Function names can contain uppercase letters, lowercase letters, numbers and
     underscores, but cannot begin with a number. We will arbitrarily limit function names
     to 20 characters. We will make their minimum length to 10 characters to avoid clashes
@@ -31,20 +33,37 @@ def make_random_function_name():
     return function_name
 
 
-def create_function(fn_name=None):
+def create_function(fn_name: str=None) -> Callable:
     fn_name = fn_name or make_random_function_name()
     exec(f'def {fn_name}(): pass')
     function = locals()[fn_name]
     return function
 
 
-def make_random_string(no_whitespace=False):
+def make_random_string(no_whitespace: bool=False) -> str:
     characters = string.ascii_letters + string.digits + string.punctuation
     if not no_whitespace:
         characters += string.whitespace
     string_length = random.randint(1, 100)
     random_string = ''.join(random.choice(characters) for _ in range(string_length))
     return random_string
+
+
+def make_random_dir_path() -> Path:
+    """ Creates a fully-qualified Path to a fake directory. The directory path is
+    guarantee to begin with / and starts with no special characters other than /.
+    The branch_depth of the path (ie /one/two/three has a depth of 3) is random and
+    lies between 2 and 10 inclusive. Each node in the branch has between 1 and 10
+    characters, which comprise upper- and lower-case characters and numbers. """
+    characters = string.ascii_letters + string.digits
+    branch_depth = random.randint(2, 10)
+    dirs = []
+    for _ in range(branch_depth):
+        string_length = random.randint(1, 10)
+        random_string = ''.join(random.choice(characters) for _ in range(string_length))
+        dirs.append(random_string)
+    dir_str = '/' + '/'.join(dirs)
+    return Path(dir_str)
 
 
 class TargetFactory(AbstractFactory):
