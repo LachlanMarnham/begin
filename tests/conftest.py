12bb66c248cbcdce3114daa91972c0fd9f21cd3f
Lachlan Marnham <lachlan.marnham@farfetch.com>
2022-01-17 20:03:19 +0000

    Make make_random_string fixture defer to factory

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,18 +9,12 @@ from unittest import mock
 import pytest
 
 from begin.registry import Registry
+from tests.resources import factory
 
 
 @pytest.fixture(scope='function')
 def make_random_string():
-    def _make_random_string(no_whitespace=False):
-        characters = string.ascii_letters + string.digits + string.punctuation
-        if not no_whitespace:
-            characters += string.whitespace
-        string_length = random.randint(1, 100)
-        random_string = ''.join(random.choice(characters) for _ in range(string_length))
-        return random_string
-    return _make_random_string
+    return factory.make_random_string
 
 
 @pytest.fixture(scope='function')
