23cc9f4eb96d24d9392d0cef0a006c98dc9d7507
Lachlan Marnham <lachlan.marnham@farfetch.com>
2022-01-17 18:07:27 +0000

    Add TargetFactory

--- tests/resources/factory.py
+++ tests/resources/factory.py
@@ -1,9 +1,28 @@
-from abc import ABCMeta, abstractmethod
+from abc import (
+    ABCMeta,
+    abstractmethod,
+)
+
+from begin.registry import Target
 
 
 class AbstractFactory(metaclass=ABCMeta):
+
+    @staticmethod
     @abstractmethod
     def create(self):
         pass
 
 
+class TargetFactory(AbstractFactory):
+
+    @staticmethod
+    def create(**kwargs):
+        return Target(
+            function=kwargs.get('function'),
+            registry_namespace=kwargs.get('registry_namespace'),
+        )
+
+# function_name = 'abc'
+# exec(f'def {function_name}(): pass')
+# fn = locals()['abc']
