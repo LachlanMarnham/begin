99b78ec8d0f689950c748af316a69417f2bfce4f
Lachlan Marnham <lachlan.marnham@farfetch.com>
2022-01-18 10:55:43 +0000

    Add TODO for param converters

--- begin/targets.py
+++ begin/targets.py
@@ -20,3 +20,25 @@ def tests():
 def tests_with_setup():
     install()
     tests()
+
+
+# TODO command line args are all passed as strings. There should be a way to register
+# converters in the function. Without converters:
+#
+# @registry.register_target
+# def target_with_params(arg_1, arg_2):
+#     print('arg_1 and arg_2 are both strings')
+#     pass
+#
+#
+# def to_bool(val):
+#     if val.lower() == 'true' or int(val) !== 0 or .... etc:
+#         return True
+#     return False
+#
+#
+# @registry.register_target(converters={'arg_1': to_bool, 'arg_2': int})
+# def target_with_params(arg_1, arg_2):
+#     print('arg_1 and arg_2 are both strings')
+#     pass
+#
