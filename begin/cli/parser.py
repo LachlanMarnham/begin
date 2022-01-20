3e6536fa49a3658cf01236502c548ee43c5fea1f
Lachlan Marnham <lachlan.marnham@farfetch.com>
2022-01-20 16:45:09 +0000

    Add --extension and --global-dir optional args

--- begin/cli/parser.py
+++ begin/cli/parser.py
@@ -1,21 +1,37 @@
 import argparse
 
-from begin.constants import DEFAULT_REGISTRY_NAME
+# from begin.constants import DEFAULT_REGISTRY_NAME
 
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='A utility for running targets in a targets.py file.')
+
+# parser.add_argument(
+#     'target',
+#     help='The name of the target to run.',
+# )
+# parser.add_argument(
+#     '-n',
+#     '--namespace',
+#     default=DEFAULT_REGISTRY_NAME,
+#     help='The name of the Registry the target is registered with.',
+# )
 
 parser.add_argument(
-    'target',
-    help='The name of the target to run.',
+    '-e',
+    '--extension',
+    default='*targets.py',
+    help='The suffix to match target file patterns against.',
 )
+
 parser.add_argument(
-    '-n',
-    '--namespace',
-    default=DEFAULT_REGISTRY_NAME,
-    help='The name of the Registry the target is registered with.',
+    '-g',
+    '--global-dir',
+    default='home',
+    help='The location of the directory holding global targets files.',
 )
+parsed_args = parser.parse_args()
 
-args = parser.parse_args()
+print(parsed_args)
 
-print(args.target, args.namespace)
+import pdb; pdb.set_trace()
+print(parsed_args, parser)
