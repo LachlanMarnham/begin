759fdd2790c8d57276433a6f4b8ff786ba911b65
Lachlan Marnham <lachlan.marnham@farfetch.com>
2022-01-21 01:31:03 +0000

    Add class Request

--- begin/cli/parser.py
+++ begin/cli/parser.py
@@ -1,20 +1,46 @@
 import argparse
+from typing import (
+    Dict,
+    List,
+)
 
-# from begin.constants import DEFAULT_REGISTRY_NAME
+from begin.constants import DEFAULT_REGISTRY_NAME
+
+
+class Request:
+    def __init__(self, target_identifier: str) -> None:
+        target_name, _, registry_namespace = target_identifier.partition('@')
+        self._target_name = target_name
+        self._registry_namespace = registry_namespace or DEFAULT_REGISTRY_NAME
+        self._options: Dict[str, str] = {}
+
+    def add_option(self, param_identifier: str) -> None:
+        key, _, value = param_identifier.partition(':')
+        self._options[key] = value
+
+
+def parse_requests(args: List[str]) -> List[Request]:
+    # TODO things to formalise:
+    # - params must be seperated by a colon
+    # - target names and registry names must not contain a colon
+    # - target names and registry names must not contain an @
+    # - if a target, a registry or an argument contains whitespace, it must be wrapped in '...'
+    # For now, this algorithm can only handle one target request
+    requests = [] 
+    request = None
+    for arg in args:
+        if ':' not in arg:
+            # Not a key:value argument pair, must be either target or target@namespace
+            if request is not None:
+                requests.append(request)
+            request = Request(arg)
+        else:
+            request.add_option(arg)
+    requests.append(request)
+    return requests
 
 
 parser = argparse.ArgumentParser(description='A utility for running targets in a targets.py file.')
-
-# parser.add_argument(
-#     'target',
-#     help='The name of the target to run.',
-# )
-# parser.add_argument(
-#     '-n',
-#     '--namespace',
-#     default=DEFAULT_REGISTRY_NAME,
-#     help='The name of the Registry the target is registered with.',
-# )
 
 parser.add_argument(
     '-e',
@@ -29,9 +55,15 @@ parser.add_argument(
     default='home',
     help='The location of the directory holding global targets files.',
 )
-parsed_args = parser.parse_args()
+parsed_args, extra_args = parser.parse_known_args()
 
-print(parsed_args)
+def foo(key):
+    print(key)
 
+def bar(key, key_2):
+    print(key)
+
+def baz():
+    pass
 import pdb; pdb.set_trace()
-print(parsed_args, parser)
+print(parsed_args, extra_args)
