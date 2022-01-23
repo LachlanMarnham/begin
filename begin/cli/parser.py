019fc8f3cd0427313a0edb52cd6104f2477df730
Lachlan Marnham <lachlan.marnham@farfetch.com>
2022-01-23 02:08:43 +0000

    Remove TODO from parser._parse_requests

--- begin/cli/parser.py
+++ begin/cli/parser.py
@@ -64,12 +64,6 @@ class ParsedCommand:
 
 
 def _parse_requests(args: List[str]) -> List[Request]:
-    # TODO things to formalise:
-    # - params must be seperated by a colon
-    # - target names and registry names must not contain a colon
-    # - target names must not contain an @
-    # - if a target, a registry or an argument contains whitespace, it must be wrapped in '...'
-    # For now, this algorithm can only handle one target request
     requests = []
     request = None
     for arg in args:
