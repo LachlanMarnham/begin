fc3d092420da636fc6f3825d0d72f6597ef968d4
Lachlan Marnham <lachlan.marnham@farfetch.com>
2022-01-26 22:24:19 +0000

    Add test_flake8

--- tests/begin/test_recipes.py
+++ tests/begin/test_recipes.py
@@ -25,10 +25,23 @@ def test_with_exit(exit_code):
 def test_coverage(mock_coverage_main):
     exit_code = randint(0, 100)
     mock_coverage_main.return_value = exit_code
-    stub_args = ['--some', 'comand', '--line', 'args']
+    stub_args = ['--some', 'command', '--line', 'args']
 
     with pytest.raises(SystemExit) as err_info:
         recipes.coverage(*stub_args)
 
     assert mock_coverage_main.call_args_list == [mock.call(stub_args)]
+    assert err_info.value.code == exit_code
+
+
+@mock.patch('flake8.main.cli.main')
+def test_flake8(mock_flake8_main):
+    exit_code = randint(0, 100)
+    mock_flake8_main.return_value = exit_code
+    stub_args = ['--some', 'command', '--line', 'args']
+
+    with pytest.raises(SystemExit) as err_info:
+        recipes.flake8(*stub_args)
+
+    assert mock_flake8_main.call_args_list == [mock.call(stub_args)]
     assert err_info.value.code == exit_code
