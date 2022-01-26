b8c6e6ca2b9c5e75c32e003ea3ed4aba0c6ad761
Lachlan Marnham <lachlan.marnham@farfetch.com>
2022-01-26 20:27:00 +0000

    Make tests_coverage recipe correctly calculate coverage data

--- targets.py
+++ targets.py
@@ -18,6 +18,48 @@ def tests():
     pytest('--cov', 'begin')
 
 
+# TODO change recipe name to tests
+@registry.register_target
+def tests_coverage():
+    import sysconfig
+    import sys
+    import os
+    pth_dir = sysconfig.get_path("purelib")
+    pth_path = os.path.join(pth_dir, "zzz_setup.cfg")
+    with open(pth_path, "w") as pth_file:
+        pth_file.write("import coverage; coverage.process_startup()\n")
+
+    import coverage
+    cov = coverage.Coverage(config_file="setup.cfg")
+    cov._warn_unimported_source = False
+    cov._warn_preimported_source = False
+    cov.start()
+
+    import begin
+    covmods = {}
+    begin_dir = os.path.split(begin.__file__)[0]
+
+    # We have to make a list since we'll be deleting in the loop.
+    modules = list(sys.modules.items())
+    for name, mod in modules:
+        if name.startswith('begin'):
+            if getattr(mod, '__file__', "??").startswith(begin_dir):
+                covmods[name] = mod
+                del sys.modules[name]
+    import coverage                         # pylint: disable=reimported
+    # sys.modules.update(covmods)
+
+    # Run tests, with the arguments from our command line.
+    pytest('--cov', 'begin')
+
+    cov.stop()
+    os.remove(pth_path)
+    cov.combine()
+    cov.save()
+    import pdb; pdb.set_trace()
+    print(pth_path)
+
+
 @registry.register_target
 def install():
     print('default install')
